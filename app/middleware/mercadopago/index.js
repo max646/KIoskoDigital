90d363bbbcdd7a588ebe7a543d0792b614157120
omarteodoroalegre <alegreomarteodoro@gmail.com>
2015-04-14 01:21:01 -0300

    Upgrades in MP config.

--- app/middleware/mercadopago/index.js
+++ app/middleware/mercadopago/index.js
@@ -2,9 +2,7 @@ var MP = require('mercadopago');
 
 module.exports = function(app) {
     var mp = new MP(app.get('config').mercadopago.client_id, app.get('config').mercadopago.client_secret);
-    //comentado hasta lanzar en produccion
-    //mp.sandboxMode(app.get('env') !== 'production');
-    mp.sandboxMode(true);
+    mp.sandboxMode(app.get('config').mercadopago.init_point === 'sandbox');
     app.set('mp', mp);
     return mp;
 };
